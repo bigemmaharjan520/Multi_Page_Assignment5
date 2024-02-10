a532a08ba1d36f906f6dff601665612d20463d52
Bigem Maharjan <bigemmaharjan520@gmail.com>
2024-02-10 12:50:57 -0800

    Initial Commit

---
+++ Bigem_MultiPage_Layout_A5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bigem_MultiPage_Layout_A5
+//
+//  Created by user240741 on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
